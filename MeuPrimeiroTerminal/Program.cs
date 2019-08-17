e759c2835b9adb4722593be49cfa6974e8069f0d
Julia Lemes <julialemes28@outlook.com>
2019-08-17 11:45:11 -0300

    Manipulando strings

---
+++ MeuPrimeiroTerminal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeuPrimeiroTerminal
+{
+    class Program
+    {
+        private static object cont ;
+
+        static void Main(string[] args)
+        {
+             string txt1 = "Hello World!";
+             string txt2 = "Julia, com idade de:";
+             int idade = 16;
+             string txt3 = $"{txt1} {txt2} {idade}";
+            
+            Console.WriteLine(txt3);
+        }
+    }
+    
+}
